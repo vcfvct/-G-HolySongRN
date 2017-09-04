baccecee1638bf860fb826a9645b06065bd9f065
Han Li <vcfvct@gmail.com>
2017-09-04 12:05:05 -0400

    make iOS work in simulator clean some code and duplicate lyrics

--- index.android.js
+++ index.android.js
@@ -1,23 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import SongListScreen from "./src/components/SongListScreen";
 import Navigation from "./src/index";
-
-/*
-export default class HolySongRN extends Component {
-  render() {
-    return (
-        <SongListScreen/>
-    );
-  }
-
-}
- */
 
 AppRegistry.registerComponent('HolySongRN', () => Navigation);
